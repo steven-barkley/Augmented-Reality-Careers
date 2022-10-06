3b925e6f2d8be840448e35367fc2cfcd7d870430
Steven Barkley <steven.barkley@outlook.com>
2022-10-05 22:57:20 -0400

    Made a people counter

---
+++ Scrimba-JS/app.js
@@ -0,0 +1,3 @@
+console.log( "This is a ReferenceError: document is not defined" );
+var x = document.getElementById( "msg" );
+console.log( x );
---
+++ Scrimba-JS/index.js
@@ -0,0 +1,8 @@
+let count = 0;
+
+function increment() {
+    return count = count + 1;
+    // console.log( count );
+}
+
+
