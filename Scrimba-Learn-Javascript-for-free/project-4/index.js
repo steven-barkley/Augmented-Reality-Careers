89693dfda19a7506e0ebce6833ca9773a07e026e
Steven Barkley <steven.barkley@outlook.com>
2022-10-24 06:33:12 -0400

    Updated Blackjack Sum and Card output

--- Scrimba-Learn-Javascript-for-free/project-4/index.js
+++ Scrimba-Learn-Javascript-for-free/project-4/index.js
@@ -17,27 +17,33 @@ if ( sum < 21 ) {
 
 // Check if the person is old enough to enter the nightclub (21)
 // Log a suitable message to the console in both cases
-let firstCard = Math.floor( Math.random() * ( 11 - 2 ) + 2 );
-let secondCard = Math.floor( Math.random() * ( 11 - 2 ) + 2 );
+let firstCard = 10
+let secondCard = 11
 
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let message = "";
 let isAlive = true;
-// 2. Create a startGame() function. Move the conditional
-// below (line 28-39) inside the body of the function.
+
+let messageEl = document.getElementById( "message-el" );
+let sumEl = document.getElementById( "sum-el" );
+let cardsEl = document.getElementById( "cards-el" );
+
+console.log( cardsEl );
+
 function startGame() {
     if ( sum > 21 ) {
         isAlive = false;
-        message = `${ sum } You bust!!!`;
+        message = `You bust!!!`;
     } else if ( sum === 21 ) {
         hasBlackJack = true;
-        message = `${ sum }. You got Blackjack!`
+        message = `You got Blackjack!`
     } else {
-        message = `${ sum }. Hit or stay?`
+        message = `Hit or stay?`
     }
-    // if less than 21 -> "You can not enter the club!"
-    // else            -> "Welcome!"
-    console.log( message );
+    messageEl.textContent = message
+    cardsEl.innerText += `${ firstCard } ${ secondCard }`
+    //sumEl.textContent = `Sum: ${ sum }`
+    sumEl.innerText += ` ${ sum }`;
 }
 
