e18b349d6c32b927318b233e27250db89a61d947
Steven Barkley <steven.barkley@outlook.com>
2022-10-25 00:44:19 -0400

    add JS-crashcourse and main.js

---
+++ Scrimba-Learn-Javascript-for-free/js-crashcourse/main.js
@@ -0,0 +1,15 @@
+/* let result
+console.log( result ) */
+
+let x = 10
+let y = 5
+
+console.log( x + y )
+
+//Check if value are equal
+
+console.log( x == y )
+console.log( ++x )
+console.log( --y )
+
+console.log( y <= x )
